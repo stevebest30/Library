4a584996af5cc78fb10b621545c53b22dac664d0
stevebest30 <93195945+stevebest30@users.noreply.github.com>
2021-11-23 15:14:44 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+/*----------------------------
+    Project:County  Library Management system
+    Author:steve best
+    Date:25/10/2021
+    Compiler:GNU GCC
+    Language :C99
+    Licence:MIT
+---------------------------------*/
+
+
+#include <stdio.h>
+int main(){
+    printf("\t County Library Management System\n");
+    printf("1:Log in: \n");
+    printf("Welcome Mr Stev best\n");
+    printf("2:Add user: \n");
+    printf("3:Add book: \n");
+    printf("4:Delete user: \n");
+    printf("5:Delete book: \n");
+    printf("6:Update user: \n");
+    printf("8:Log out => \n");
+
+    return 0;
+}
